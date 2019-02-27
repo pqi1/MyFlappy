4ba899f7ace1a8f9397f1450b9c8978ba6cc293d
pqi1 <pqi1@binghamton.edu>
2019-02-27 09:44:52 -0500

    bird can fly. almost done

--- core/src/com/qi/flappy/sprites/Bird.java
+++ core/src/com/qi/flappy/sprites/Bird.java
@@ -1,45 +1,66 @@
 package com.qi.flappy.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class Bird {
     private static final int GRAVITY = -5;
+    private static final int MOVEMENT = 100;
+
     private Vector3 position;
     private Vector3 velocity;
-    private Texture birdTexture;
+    private Rectangle bounds;
+    private Texture texture;
+    private Animation birdAnimation;
 
     public Bird(int x, int y){
         position = new Vector3(x, y, 0);
         velocity = new Vector3(0, 0, 0);
-        birdTexture = new Texture("bird.png");
+        texture  = new Texture("birdanimation.png");
+        birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
+
+        bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight() / 3);
     }
 
     public void update(float dt){
+        birdAnimation.update(dt);
         if(position.y > 0){
             velocity.add(0, GRAVITY, 0);
         }
         velocity.scl(dt);
-        position.add(0, velocity.y, 0);
+        position.add(MOVEMENT * dt, velocity.y, 0);
 
         if(position.y < 0){
             position.y = 0;
         }
 
         velocity.scl(1/dt);
+
+        bounds.setPosition(position.x, position.y);
     }
 
     public Vector3 getPosition() {
         return position;
     }
 
-    public Texture getBirdTexture() {
-        return birdTexture;
+    public TextureRegion getBirdTexture() {
+        return birdAnimation.getFrame();
     }
 
     public void jump(){
-        velocity.y = 250;
+        velocity.y = 150;
     }
+
+    public Rectangle getBounds(){
+        return bounds;
+    }
+
+    public void dispose(){
+        texture.dispose();
+    }
+
 }
 
 
--- core/src/com/qi/flappy/sprites/Tube.java
+++ core/src/com/qi/flappy/sprites/Tube.java
@@ -1,16 +1,20 @@
 package com.qi.flappy.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.Random;
 
 public class Tube {
+    public static final int TUBE_WIDTH = 52;
     private static final int FLUCTUATION = 130;
     private static final int TUBE_GAP = 100;
     private static final int LOWEST_OPENING = 120;
     private Texture topTube, botTube;
     private Vector2 posTopTube, posBotTube;
+    private Rectangle boundsTop, boundsBot;
+
     private Random rand;
 
     public Tube(float x){
@@ -20,6 +24,9 @@ public class Tube {
 
         posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
         posBotTube = new Vector2(x, posTopTube.y -  TUBE_GAP - botTube.getHeight());
+
+        boundsTop = new Rectangle(posTopTube.x, posTopTube.y, topTube.getWidth(), topTube.getHeight());
+        boundsBot = new Rectangle(posBotTube.x, posBotTube.y, botTube.getWidth(), botTube.getHeight());
     }
 
     public Texture getTopTube() {
@@ -37,4 +44,23 @@ public class Tube {
     public Vector2 getPosBotTube() {
         return posBotTube;
     }
+
+    public void reposition(float x){
+        posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube.set(x, posTopTube.y -  TUBE_GAP - botTube.getHeight());
+
+        boundsTop.setPosition(posTopTube.x,posTopTube.y);
+        boundsBot.setPosition(posBotTube.x,posBotTube.y);
+    }
+
+    public boolean collides(Rectangle player){
+        return player.overlaps(boundsTop) || player.overlaps(boundsBot);
+
+    }
+
+    public void dispose(){
+        topTube.dispose();
+        botTube.dispose();
+    }
+
 }
