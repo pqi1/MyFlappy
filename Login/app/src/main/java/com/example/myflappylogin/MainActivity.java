df940dbadc649bc0fe0b6b4fd6ce97d53ade2ee3
pqi1 <pqi1@binghamton.edu>
2019-03-15 23:46:58 -0400

    every thing done

--- Login/app/src/main/java/com/example/myflappylogin/MainActivity.java
+++ Login/app/src/main/java/com/example/myflappylogin/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
